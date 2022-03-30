e77c179bbc521eb06e9bd1e9f7cb12c95393c40e
Tomas Guttman <tomas.guttman@sansano.usm.cl>
2022-03-30 18:37:17 -0300

    ✨ Header added

---
+++ 02. budget/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import NewBudget from './NewBudget'
+import BudgetControl from './BudgetControl'
+
+const Header = ({
+  budget, 
+  setBudget, 
+  isValidBudget, 
+  setIsValidBudget
+}) => {
+  return (
+    <header>
+      <h1>Planificador de Gastos</h1>
+      {isValidBudget ? (
+        <BudgetControl 
+          budget={budget}
+        />
+      ) : (
+        <NewBudget
+          budget={budget}
+          setBudget={setBudget}
+          setIsValidBudget={setIsValidBudget}
+        />
+      )}
+    </header>
+  )
+}
+
+export default Header
