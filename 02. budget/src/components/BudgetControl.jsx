3d74fae3f4a5d7774c66fa731f39ac25f8f62e09
Tomas Guttman <tomas.guttman@sansano.usm.cl>
2022-04-04 20:24:15 -0400

    ✨ add available and spent totals

--- 02. budget/src/components/BudgetControl.jsx
+++ 02. budget/src/components/BudgetControl.jsx
@@ -1,8 +1,15 @@
-const BudgetControl = ({budget}) => {
-  const formatAmount = amount => amount.toLocaleString('es-CL',{
-    style: 'currency',
-    currency: 'CLP'
-  })
+import { useEffect, useState } from "react"
+import { formatAmount } from "../helpers"
+
+const BudgetControl = ({budget, spendings}) => {
+  const [available, setAvailable] = useState(0)
+  const [spent, setSpent] = useState(0)
+  useEffect(()=> {
+    const totalSpent = spendings.reduce((total, spentAux) => spentAux.amount + total,0)
+    setSpent(totalSpent)
+    const totalAvailabe = budget - totalSpent
+    setAvailable(totalAvailabe)
+  },[spendings])
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
       <div>
@@ -10,8 +17,8 @@ const BudgetControl = ({budget}) => {
       </div>
       <div className="contenido-presupuesto">
         <p><span>Presupuesto:</span> {formatAmount(budget)}</p>
-        <p><span>Disponible:</span> {formatAmount(0)}</p>
-        <p><span>Gastado:</span> {formatAmount(0)}</p>
+        <p><span>Disponible:</span> {formatAmount(available)}</p>
+        <p><span>Gastado:</span> {formatAmount(spent)}</p>
       </div>
     </div>
   )
