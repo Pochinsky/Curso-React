1c50fecf2c90be94426546bd5005d8e8de78f6e7
Tomás Benjamín Guttman Pogorzelski <tomas.guttman@alumnos.inf.utfsm.cl>
2022-09-07 05:47:18 -0300

    🔥 remove projects doesn't correspond

--- 01. cites/src/components/Error.jsx
+++
@@ -1,9 +0,0 @@
-const Error = ({ message }) => {
-  return (
-    <div className="bg-red-800 text-white text-center mb-10 p-3 uppercase font-bold rounded">
-      <p>{message}</p>
-    </div>
-  )
-}
-
-export default Error
--- 03. criptos/src/data/coins.js
+++
@@ -1,9 +0,0 @@
-const coins = [
-  { id: "USD", name: "Dolar de Estados Unidos" },
-  { id: "CLP", name: "Peso Chileno" },
-  { id: "MXN", name: "Peso Mexicano" },
-  { id: "EUR", name: "Euro" },
-  { id: "GBP", name: "Libra Esterlina" },
-];
-
-export { coins };
